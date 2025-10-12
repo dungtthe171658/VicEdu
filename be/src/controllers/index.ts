618c77bc752468566adebcb378404f843cacaea0
HE171455 <134669954+hungbeep@users.noreply.github.com>
2025-10-12 20:56:46 +0700

    update be
    update be

--- be/src/controllers/index.ts
+++ be/src/controllers/index.ts
@@ -1,1 +1,14 @@
-export * from "./auth.controller";
+// src/controllers/index.ts
+
+export * from './auth.controller';
+export * from './user.controller';
+export * from './book.controller';
+export * from './category.controller';
+export * from './course.controller';
+export * from './lesson.controller';
+export * from './otp.controller';
+export * from './order.controller';
+export * from './payment.controller';
+export * from './quiz.controller';
+export * from './review.controller';
+export * from './dashboard.controller';
